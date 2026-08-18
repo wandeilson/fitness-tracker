34d98649287b9fc7a2c17f9a350afd5d83a49166
Wandeilson Gomes da Silva <wandeilsongomes@gmail.com>
2026-08-18 09:47:53 -0300

    Ajustes login

--- frontend/src/app/auth/auth.guard.ts
+++ frontend/src/app/auth/auth.guard.ts
@@ -1,10 +1,16 @@
-import { inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { inject, PLATFORM_ID } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 export const authGuard: CanActivateFn = () => {
   const authService = inject(AuthService);
   const router = inject(Router);
+  const platformId = inject(PLATFORM_ID);
+
+  if (!isPlatformBrowser(platformId)) {
+    return true;
+  }
 
   if (authService.isAuthenticated()) {
     return true;
